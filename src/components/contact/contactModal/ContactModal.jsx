e06bfdc5876b13ceeee66c190230d863a68a54f1
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-10 10:06:42 -0500

    fixed: api base url is now pointing to the right api end point

--- src/components/contact/contactModal/ContactModal.jsx
+++ src/components/contact/contactModal/ContactModal.jsx
@@ -24,8 +24,8 @@ const ContactRepModal = ({ open, onClose }) => {
 
     try {
       const response = await axiosInstance.post(
-        // `${process.env.REACT_APP_PYTHONANYWHERE_API}/provider/contact-rep/`,
-        `${process.env.REACT_APP_API_URL}/provider/contact-rep/`,
+        `${process.env.REACT_APP_PYTHONANYWHERE_API}/provider/contact-rep/`,
+        // `${process.env.REACT_APP_API_URL}/provider/contact-rep/`,
         formData
       );
       console.log("Message sent successfully:", response.data);
